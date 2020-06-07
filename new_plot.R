fe7344bb63b0fb380d628032a6445f368bd7cd90
ssjuliana <julianass.estatistica@gmail.com>
2020-06-06 21:59:30 -0300

    dashboard rs

--- new_plot.R
+++ new_plot.R
@@ -28,12 +28,40 @@ options_cluster <- c('Município', 'Região COVID')
 # ----------------------------------------------
 # variável sexo: 
 
-# 
-
-
-gg_sex <- function(input){
+# default é casos confirmados e por município
+gg_sex <- function(input = options_var[1], cluster = options_cluster[1]){
+  
+  # definir se o banco é de municipio ou regiao covid:
+  
+  if(cluster != options_cluster[1]){
+    
+    temp <- dados_covid_rs %>% 
+      select(regiao_covid, sexo, evolucao) 
+    
+  } else {
+     
+    temp <- dados_covid_rs %>% 
+      select(municipio, sexo, evolucao)
+  }
+  
+  names(temp)[1] <- 'variavel'
+  
+  # agregar os dados: 
+  df <- data.frame(table(temp$variavel, temp$sexo, temp$evolucao))
+  names(df) <- c(names(temp), 'Contagem')
+  
+  
   
 }
 
-temp <- dados_covid_rs %>% 
-  select(municipio, regiao_covid, sexo, )
+
+ggplot(temp, aes(y = variavel)) +
+  geom_bar(aes(fill = sexo), position = position_stack(reverse = TRUE)) +
+  theme(legend.position = "top")
+
+
+
+
+
+
+
