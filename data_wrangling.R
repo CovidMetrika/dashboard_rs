cf6fac72d7889e2bf186906ed468255a323573ab
Gustavo Machado Utpott <gustavo.utpott@gmail.com>
2020-06-12 20:22:40 -0300

    att diária leitos

--- data_wrangling.R
+++ data_wrangling.R
@@ -187,7 +187,7 @@ arquivos_troca_nome <- c("leitos_dados_ses_05_05.csv","leitos_dados_ses_06_05.cs
                          "leitos_dados_ses_01_06.csv","leitos_dados_ses_02_06.csv","leitos_dados_ses_03_06.csv",
                          "leitos_dados_ses_04_06.csv","leitos_dados_ses_05_06.csv","leitos_dados_ses_06_06.csv",
                          "leitos_dados_ses_07_06.csv","leitos_dados_ses_08_06.csv","leitos_dados_ses_09_06.csv",
-                         "leitos_dados_ses_10_06.csv","leitos_dados_ses_11_06.csv")
+                         "leitos_dados_ses_10_06.csv","leitos_dados_ses_11_06.csv","leitos_dados_ses_12_06.csv")
 caminhos_troca_nome <- str_c(pasta,arquivos_troca_nome)
 
 arruma_nome <- map(caminhos_troca_nome, read_csv) %>%
@@ -230,7 +230,7 @@ aux <- leitos_uti %>%
   mutate(hospital_unico = str_c(hospital,"_",cnes)) %>%
   select(-n)
 
-hospitais_troca <- unique(teste$hospital)
+hospitais_troca <- unique(aux$hospital)
 
 leitos_uti <- leitos_uti %>%
   left_join(aux, by = c("hospital","cnes")) %>%
